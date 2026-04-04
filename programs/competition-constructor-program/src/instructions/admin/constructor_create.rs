5ebd884909754b52746a608876e186b3b62b67a5
None <destis_Andromeda@proton.me>
2026-04-04 22:24:23 +0200

    feat(constructor_create): add ConstructorCreate accounts struct

---
+++ programs/competition-constructor-program/src/instructions/admin/constructor_create.rs
@@ -0,0 +1,44 @@
+use anchor_lang::prelude::*;
+
+use crate::state::*;
+use crate::seeds::*;
+use crate::error::*;
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ConstructorCreateArgs {
+    // in lamports
+    pub transaction_fee: u64,
+}
+
+#[derive(Accounts)]
+pub struct ConstructorCreate<'info> {
+    #[account(mut)]
+    pub creator_key: Signer<'info>,
+
+    #[account(
+        init,
+        payer = creator_key,
+        space = 8 + Constructor::INIT_SPACE,
+        seeds = [
+            SEED_PREFIX,
+            program_config.key().as_ref(),
+            SEED_CONSTRUCTOR,
+            creator_key.key().as_ref(),
+        ],
+        bump,
+    )]
+    pub constructor: Account<'info, Constructor>,
+
+    #[account(
+        has_one = creator_key
+            @ CustomError::Unauthorized,
+        seeds = [
+            SEED_PREFIX,
+            SEED_PROGRAM_CONFIG,
+        ],
+        bump  = program_config.bump,
+    )]
+    pub program_config: Account<'info, ProgramConfig>,
+
+    pub system_program: Program<'info, System>,
+}
