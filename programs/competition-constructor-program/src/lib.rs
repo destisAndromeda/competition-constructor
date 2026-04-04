e076da8f2c60a5d4f75fe0a1c2e97af618dc2a4f
None <destis_Andromeda@proton.me>
2026-04-04 18:48:37 +0200

    feat(lib): add templates for instructions

--- programs/competition-constructor-program/src/lib.rs
+++ programs/competition-constructor-program/src/lib.rs
@@ -3,15 +3,263 @@ use anchor_lang::prelude::*;
 mod error;
 mod seeds;
 mod state;
-mod instructions;
-
-use instructions::*;
 
 declare_id!("8bDwCU1Y598BoD2BWnchBCeuvQ4rW3DUbV4c3tDvRkVX");
 
 #[program]
 pub mod competition_constructor_program {
+    #![allow(unused_variables)]
+
     use super::*;
 
+    pub fn program_config_init(
+        ctx: Context<ProgramConfigInit>,
+        args: ProgramConfigInitArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn program_config_update_creator_key(
+        ctx: Context<ProgramConfigUpdateCreatorKey>,
+        args: ProgramConfigUpdateCreatorKeyArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn program_config_update_treasury(
+        ctx: Context<ProgramConfigUpdateTreasury>,
+        args: ProgramConfigUpdateTreasuryArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn constructor_init(ctx: Context<ConstructorInit>, args: ConstructorInitArgs) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn transaction_fee_update(
+        ctx: Context<TransactionFeeUpdate>,
+        args: TransactionFeeUpdateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_create(
+        ctx: Context<SwissSystemCreate>,
+        args: SwissSystemCreateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_add_participant(
+        ctx: Context<SwissSystemAddParticipant>,
+        args: SwissSystemAddParticipantArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_determine_winner(
+        ctx: Context<SwissSystemDetermineWinner>,
+        args: SwissSystemDetermineWinnerArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_change_registration_period(
+        ctx: Context<SwissSystemChangeRegistrationPeriod>,
+        args: SwissSystemChangeRegistrationPeriodArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_change_competition_period(
+        ctx: Context<SwissSystemChangeCompetitionPeriod>,
+        args: SwissSystemChangeCompetitionPeriodArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_change_withdraw_period(
+        ctx: Context<SwissSystemChangeWithdrawPeriod>,
+        args: SwissSystemChangeWithdrawPeriodArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn swiss_system_update_status(
+        ctx: Context<SwissSystemUpdateStatus>,
+        args: SwissSystemUpdateStatusArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn vault_create(ctx: Context<VaultCreate>, args: VaultCreateArgs) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn vault_update_status(
+        ctx: Context<VaultUpdateStatus>,
+        args: VaultUpdateStatusArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn vault_withdraw(ctx: Context<VaultWithdraw>, args: VaultWithdrawArgs) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn leader_board_create(
+        ctx: Context<LeaderBoardCreate>,
+        args: LeaderBoardCreateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn leader_board_update(
+        ctx: Context<LeaderBoardUpdate>,
+        args: LeaderBoardUpdateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn participant_create(
+        ctx: Context<ParticipantCreate>,
+        args: ParticipantCreateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn participant_status_update(
+        ctx: Context<ParticipantStatusUpdate>,
+        args: ParticipantStatusUpdateArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn participant_award_points(
+        ctx: Context<ParticipantAwardPoints>,
+        args: ParticipantAwardPointsArgs,
+    ) -> Result<()> {
+        Ok(())
+    }
 }
 
+#[derive(Accounts)]
+pub struct ProgramConfigInit {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ProgramConfigInitArgs {}
+
+#[derive(Accounts)]
+pub struct ProgramConfigUpdateCreatorKey {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ProgramConfigUpdateCreatorKeyArgs {}
+
+#[derive(Accounts)]
+pub struct ProgramConfigUpdateTreasury {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ProgramConfigUpdateTreasuryArgs {}
+
+#[derive(Accounts)]
+pub struct ConstructorInit {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ConstructorInitArgs {}
+
+#[derive(Accounts)]
+pub struct TransactionFeeUpdate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct TransactionFeeUpdateArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemCreate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemCreateArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemAddParticipant {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemAddParticipantArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemDetermineWinner {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemDetermineWinnerArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemChangeRegistrationPeriod {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemChangeRegistrationPeriodArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemChangeCompetitionPeriod {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemChangeCompetitionPeriodArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemChangeWithdrawPeriod {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemChangeWithdrawPeriodArgs {}
+
+#[derive(Accounts)]
+pub struct SwissSystemUpdateStatus {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct SwissSystemUpdateStatusArgs {}
+
+#[derive(Accounts)]
+pub struct VaultCreate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct VaultCreateArgs {}
+
+#[derive(Accounts)]
+pub struct VaultUpdateStatus {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct VaultUpdateStatusArgs {}
+
+#[derive(Accounts)]
+pub struct VaultWithdraw {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct VaultWithdrawArgs {}
+
+#[derive(Accounts)]
+pub struct LeaderBoardCreate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct LeaderBoardCreateArgs {}
+
+#[derive(Accounts)]
+pub struct LeaderBoardUpdate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct LeaderBoardUpdateArgs {}
+
+#[derive(Accounts)]
+pub struct ParticipantCreate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ParticipantCreateArgs {}
+
+#[derive(Accounts)]
+pub struct ParticipantStatusUpdate {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ParticipantStatusUpdateArgs {}
+
+#[derive(Accounts)]
+pub struct ParticipantAwardPoints {}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub struct ParticipantAwardPointsArgs {}
