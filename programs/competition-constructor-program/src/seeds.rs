b5f7b44079b8585ab7e5ab1a775665523fea3cc9
None <destis_Andromeda@proton.me>
2026-04-04 21:20:57 +0200

    feat(seeds): add consts seeds

---
+++ programs/competition-constructor-program/src/seeds.rs
@@ -0,0 +1,6 @@
+pub const SEED_PREFIX:         &[u8] = b"competition_constructor";
+pub const SEED_PROGRAM_CONFIG: &[u8] = b"program_config";
+pub const SEED_CONSTRUCTOR:    &[u8] = b"constructor";
+// ... Next seeds can be added in competition branches
+// SEED_SWISS_SYSTEM
+// ROUND_ROBIN etc.
