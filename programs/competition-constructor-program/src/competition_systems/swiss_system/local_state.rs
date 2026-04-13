4e04aeacea2e78127133ca08236008509f1d3e1d
None <destis_Andromeda@proton.me>
2026-04-13 17:34:05 +0200

    feat(local_state): add swiss_system_leader_board state

--- programs/competition-constructor-program/src/competition_systems/swiss_system/local_state.rs
+++ programs/competition-constructor-program/src/competition_systems/swiss_system/local_state.rs
@@ -1,5 +1,7 @@
 pub mod swiss_system;
 pub mod swiss_system_vault;
+pub mod swiss_system_leader_board;
 
 pub use swiss_system::*;
 pub use swiss_system_vault::*;
+pub use swiss_system_leader_board::*;
---
+++ programs/competition-constructor-program/src/competition_systems/swiss_system/local_state/swiss_system_leader_board.rs
@@ -0,0 +1,41 @@
+use anchor_lang::prelude::*;
+
+const LEADER_BOARD_LIMIT: usize = 30; 
+
+#[account]
+#[derive(InitSpace)]
+pub struct LeaderBoard {
+    /// Authority that can change points count in ParticiapntData
+    pub authority: Pubkey,
+
+    /// List of participants
+    #[max_len(LEADER_BOARD_LIMIT)]
+    pub participants: Vec<ParticipantData>,
+
+    /// Bump for Leaderboard account PDA seeds
+    pub bump: u8,
+}
+
+impl LeaderBoard {
+    pub fn sort_by_points(&mut self) -> Result<()> {
+        let participants = &mut self.participants[.. self.limit as usize];
+
+        participants.sort_unstable_by(|a, b| b.points.cmp(&a.points));
+
+        Ok(())
+    }
+}
+
+#[derive(
+    AnchorSerialize,
+    AnchorDeserialize,
+    InitSpace,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+)]
+pub struct ParticipantData {
+    pub address: Option<Pubkey>,
+    pub points:  u64,
+}
